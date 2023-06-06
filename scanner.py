3ce875d87be08b2341e4df530e7aeb7f08e037c9
jknoxdev <techbiotic@posteo.de>
2023-06-06 09:20:24 -0400

    added each device to be drawn while scanning

--- scanner.py
+++ scanner.py
@@ -20,16 +20,17 @@ def check_device(ip, G):
         G.add_node(ip)
         G.nodes[ip]['label'] = resolve_dns(ip)
         print(f"Device at {ip} is up.")
+        return ip
 
-    else:
-        try:
-            ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=1, verbose=False)
-            for _, rcv in ans:
-                mac = rcv[Ether].src
-                G.add_node(mac)
-                print(f"Device with MAC address {mac} discovered.")
-        except Exception as e:
-            print(f"Error while sending ARP request to {ip}: {e}")
+    try:
+        ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=1, verbose=False)
+        for _, rcv in ans:
+            mac = rcv[Ether].src
+            G.add_node(mac)
+            print(f"Device with MAC address {mac} discovered.")
+            return mac
+    except Exception as e:
+        print(f"Error while sending ARP request to {ip}: {e}")
 
 def scan_network_topology(ip_range):
     G = nx.Graph()
@@ -49,16 +50,24 @@ def scan_network_topology(ip_range):
     print("Scanning network...")
     with ThreadPoolExecutor() as executor:
         check_partial = partial(check_device, G=G)
-        executor.map(check_partial, [ip_range + '.' + str(i) for i in range(1, 256)])
+        futures = [executor.submit(check_partial, ip_range + '.' + str(i)) for i in range(1, 256)]
+
+        # Update the network diagram as devices are confirmed
+        for future in futures:
+            device = future.result()
+            if device is not None:
+                pos = nx.spring_layout(G)
+                nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=8)
+                nx.draw_networkx_labels(G, pos, labels=nx.get_node_attributes(G, 'label'), font_size=8)
+                plt.show()
+                plt.pause(0.001)
 
     # Drop root permission
     os.seteuid(os.getuid())
     print("Root permission dropped.")
 
-    # Update the network diagram
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=8)
-    nx.draw_networkx_labels(G, pos, labels=nx.get_node_attributes(G, 'label'), font_size=8)
+    # Final network diagram
+    plt.ioff()  # Disable interactive mode
     plt.show()
 
 if __name__ == '__main__':
