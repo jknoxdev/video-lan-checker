b33cbcbca573e88dd25edf946c85b0a2ad4bcce9
jknoxdev <techbiotic@posteo.de>
2023-06-06 09:09:13 -0400

    added sudo user perms for network checks, reg user for graphics

--- scanner.py
+++ scanner.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from pythonping import ping
 from scapy.all import srp, Ether, ARP
 import socket
+import os
 
 def resolve_dns(ip):
     try:
@@ -19,6 +20,9 @@ def scan_network_topology(ip_range):
     nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=8)
     plt.show()
     plt.pause(0.001)
+
+    # Request root permission for network checks
+    os.seteuid(0)
 
     # Scan IP range and add devices to the graph
     for i in range(1, 256):
@@ -55,10 +59,19 @@ def scan_network_topology(ip_range):
             except Exception as e:
                 print(f"Error while sending ARP request to {ip}: {e}")
 
+    # Drop root permission
+    os.seteuid(os.getuid())
+
     # Final network diagram
     plt.ioff()  # Disable interactive mode
     plt.show()
 
 if __name__ == '__main__':
     ip_range = '192.168.0'  # Change this to your desired IP range
+
+    # Request root permission if not already running as root
+    if os.geteuid() != 0:
+        args = [sys.executable] + sys.argv
+        os.execlp('sudo', 'sudo', *args)
+
     scan_network_topology(ip_range)
