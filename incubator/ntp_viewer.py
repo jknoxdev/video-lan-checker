ceb5742bc5baed50fd166b08f004902d70536f06
jknoxdev <techbiotic@posteo.de>
2023-07-07 16:59:28 -0400

    ntp_viewer init

---
+++ incubator/ntp_viewer.py
@@ -0,0 +1,4 @@
+  /home/jknox/code/jknoxdev/video-lan-checker/incubator:
+  total used in directory 8 available 162.4 GiB
+  drwxrwxr-x 2 jknox jknox 4096 Jul  7 16:27 .
+  drwxrwxr-x 9 jknox jknox 4096 Jul  7 16:28 ..
