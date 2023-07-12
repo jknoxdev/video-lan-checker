b12e9f45d2c4b18cb80acee9ad0e1b0bbc14f457
jknoxdev <techbiotic@posteo.de>
2023-07-11 22:19:10 -0400

    added pause on spacebar, updated readme

---
+++ incubator/7seg_004.py
@@ -0,0 +1,92 @@
+import pygame
+import time
+
+# Initialize pygame
+pygame.init()
+
+# Define colors
+DARK_GREY = (40, 40, 40)
+RED = (255, 0, 0)
+
+# Set the screen dimensions
+screen_width = 800
+screen_height = 600
+
+# Set the size of the clock display
+clock_width = int(screen_width * 0.8)
+clock_height = int(screen_height * 0.8)
+
+# Calculate the position of the clock display
+clock_x = (screen_width - clock_width) // 2
+clock_y = (screen_height - clock_height) // 2
+
+# Set the frame rate (60fps)
+fps = 60
+clock = pygame.time.Clock()
+
+# Create the screen
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+pygame.display.set_caption("LED 7-Segment Clock")
+
+# Load font
+font = pygame.font.Font(None, 200)
+font = pygame.font.Font("./fonts/7SEGMENTALDIGITALDISPLAY.ttf", 200)
+
+# Function to draw the clock display
+def draw_clock_display():
+    # Clear the screen with dark grey color
+    screen.fill(DARK_GREY)
+
+    # Render the current time as text
+    # Include milliseconds
+    current_time = time.strftime("%H:%M:%S:") + str(int(time.time() * 1000) % 1000).zfill(3)    
+    text = font.render(current_time, True, RED)
+
+    # Get the dimensions of the rendered text
+    text_rect = text.get_rect()
+
+    # Center the text in the clock display
+    text_rect.center = (clock_x + clock_width // 2, clock_y + clock_height // 2)
+
+    # Draw the text
+    screen.blit(text, text_rect)
+
+    # Update the display
+    pygame.display.flip()
+
+# Main game loop
+running = True
+paused = False
+while running:
+    # Check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.VIDEORESIZE:
+            screen_width = event.w
+            screen_height = event.h
+            screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+            clock_width = int(screen_width * 0.8)
+            clock_height = int(screen_height * 0.8)
+            clock_x = (screen_width - clock_width) // 2
+            clock_y = (screen_height - clock_height) // 2
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                paused = not paused
+
+    # Get the current time
+    current_time = time.strftime("%H:%M:%S:") + str(int(time.time() * 1000) % 1000).zfill(3)
+
+    # Only update and draw the clock display if not paused
+    if not paused:
+        # Draw the clock display
+        draw_clock_display()
+
+        # Print the current time in the console
+        print(current_time)
+
+    # Limit the frame rate
+    clock.tick(fps)
+
+# Quit the program
+pygame.quit()
