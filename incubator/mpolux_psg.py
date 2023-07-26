4654f4f68df2f0a086ee060ce79ed89367ff79a3
jknoxdev <techbiotic@posteo.de>
2023-07-25 20:23:55 -0400

    added readme, switching to 7seg

--- incubator/mpolux_psg.py
+++ incubator/mpolux_psg.py
@@ -1,16 +1,26 @@
 import PySimpleGUI as sg
 
+num_rows, num_cols = 2, 4  # Initial grid size
+
 def create_grid(num_boxes):
-    layout = [[sg.Canvas(size=(100, 40), background_color='white', pad=(10, 10), key=f'cell_{i}_{j}') for j in range(num_boxes // 2)] for i in range(2)]
+    layout = [[sg.Button('', size=(8, 2), key=f'cell_{i}_{j}', disabled=True) for j in range(num_cols)] for i in range(num_rows)]
     return layout
 
+def clear_grid():
+    for i in range(num_rows):
+        for j in range(num_cols):
+            window[f'cell_{i}_{j}'].update('')
+
 def on_channel_button_click(channel_count):
+    global num_rows, num_cols  # Access the global grid size variables
     num_boxes = int(channel_count.split("-")[0])  # Extract the number of boxes from the button text
+    clear_grid()
+    num_rows = (num_boxes + num_cols - 1) // num_cols  # Calculate the new number of rows
     layout = create_grid(num_boxes)
     window['-GRID-'].update(layout)
     print(f"{channel_count} button pressed.")
 
-sg.theme("DarkBrown")  # Setting theme to None uses the system default theme
+sg.theme("Dark")  # Setting theme to None uses the system default theme
 
 layout = [
     [sg.Text("mpo lux", font=("Arial", 20))],
@@ -33,7 +43,7 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event.startswith('cell_'):
-        print(f"Cell clicked: {event.split('_')[1:]}")  # Extract row and column indices and print
+        print(f"Cell clicked: {event.split('_')[1]}")  # Extract row and column indices and print
     elif event in ["8-channel", "12-channel", "16-channel", "24-channel"]:
         on_channel_button_click(event)
 
