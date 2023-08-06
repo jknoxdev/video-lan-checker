6cdf1c8c5fc3bf808138da06fbd4587ace7259ea
jknoxdev <techbiotic@posteo.de>
2023-08-06 16:48:29 -0400

    changed again to use pyqt5

--- incubator/ecdh.py
+++ incubator/ecdh.py
@@ -1,13 +1,13 @@
 import ecdsa
 import os
 import qrcode
-import tkinter as tk
 from PIL import Image
-import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtGui import QPixmap
 
-class ECDSApp:
-    def __init__(self, window):
-        self.window = window
+class ECDSApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
         self.load_images()
         self.create_labels()
@@ -18,43 +18,39 @@ class ECDSApp:
         self.elliptical_curve_qr_image = Image.open("keys/elliptical_curve_qr_code.png")
 
     def create_labels(self):
-        private_key_qr_label = tk.Label(self.window, text="Private Key QR Code", bg="black", fg="white")
-        private_key_qr_label.place(x=0, y=200)
+        layout = QVBoxLayout()
 
-        public_key_qr_label = tk.Label(self.window, text="Public Key QR Code", bg="black", fg="white")
-        public_key_qr_label.place(x=0, y=400)
+        private_key_qr_label = QLabel("Private Key QR Code")
+        private_key_qr_pixmap = QPixmap.fromImage(self.convert_pil_to_qimage(self.private_key_qr_image))
+        private_key_qr_label.setPixmap(private_key_qr_pixmap)
+        layout.addWidget(private_key_qr_label)
 
-        elliptical_curve_qr_label = tk.Label(self.window, text="Elliptical Curve QR Code", bg="black", fg="white")
-        elliptical_curve_qr_label.place(x=0, y=600)
+        public_key_qr_label = QLabel("Public Key QR Code")
+        public_key_qr_pixmap = QPixmap.fromImage(self.convert_pil_to_qimage(self.public_key_qr_image))
+        public_key_qr_label.setPixmap(public_key_qr_pixmap)
+        layout.addWidget(public_key_qr_label)
+
+        elliptical_curve_qr_label = QLabel("Elliptical Curve QR Code")
+        elliptical_curve_qr_pixmap = QPixmap.fromImage(self.convert_pil_to_qimage(self.elliptical_curve_qr_image))
+        elliptical_curve_qr_label.setPixmap(elliptical_curve_qr_pixmap)
+        layout.addWidget(elliptical_curve_qr_label)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def convert_pil_to_qimage(self, pil_image):
+        image_data = pil_image.convert("RGBA").tobytes("raw", "RGBA")
+        qimage = QImage(image_data, pil_image.width, pil_image.height, QImage.Format_RGBA8888)
+        return qimage
 
 def main():
-    root = tk.Tk()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    window = tk.Tk()
-    window.title("Elliptical Curve Digital Signature Algorithm")
-    window.geometry(str(screen_width-100) + "x" + str(screen_height-100))
-    window.configure(background='black')
-
-    app = ECDSApp(window)
-
-    plt.figure(figsize=(3, 3))
-    plt.imshow(app.private_key_qr_image)
-    plt.axis('off')
-    plt.show()
-
-    plt.figure(figsize=(3, 3))
-    plt.imshow(app.public_key_qr_image)
-    plt.axis('off')
-    plt.show()
-
-    plt.figure(figsize=(3, 3))
-    plt.imshow(app.elliptical_curve_qr_image)
-    plt.axis('off')
-    plt.show()
-
-    window.mainloop()
+    app = QApplication([])
+    window = ECDSApp()
+    window.setWindowTitle("Elliptical Curve Digital Signature Algorithm")
+    window.setGeometry(100, 100, 800, 600)
+    window.show()
+    app.exec_()
 
 if __name__ == "__main__":
     main()
